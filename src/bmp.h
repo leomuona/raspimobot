545c4a68a458dd1006487c23e6de93fb364df2b0
Jesse Niemistö <jesse.niemisto@cs.helsinki.fi>
2014-01-31 23:44:22 +0200

    BMP file reader/writer.

---
+++ src/bmp.h
@@ -0,0 +1,33 @@
+#ifndef BMP_H
+#define BMP_H
+
+struct RGB
+{
+	char r, g, b;
+};
+typedef struct RGB RGB;
+
+struct BMP
+{
+	int width, height;
+	int filesize;
+
+	int fluff;
+
+	char *data;
+	int data_size;
+};
+typedef struct BMP BMP;
+
+void BMP_init(BMP *bmp, int width, int height);
+void BMP_free(BMP *bmp);
+
+void BMP_put(BMP *bmp, int x, int y, RGB *rgb);
+
+void BMP_get(BMP *bmp, int x, int y, RGB *rgb);
+
+void BMP_write(BMP *bmp, const char *filename);
+
+void BMP_read(BMP *bmp, const char *filename);
+
+#endif // BMP_H
