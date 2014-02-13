faf396b49b3326ba08c3a54a7e1973313af658c0
Matias Hilden <domewolf@gmail.com>
2014-02-13 16:35:16 +0200

    Added rotation angle calculation.

--- src/camera.c
+++ src/camera.c
@@ -5,6 +5,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#ifndef M_PI
+#define M_PI 3.14159265359f
+#endif
+
+#define CAMERA_FOV_VERTICAL 41*(M_PI/180)
+#define CAMERA_FOV_HORIZONTAL 54*(M_PI/180)
+
 void read_output(FILE *fd, size_t count, char *buffer)
 {
 	size_t left = count;
@@ -42,6 +49,50 @@ int take_pic(BMP *bmp)
 		memcpy(bmp->data+w*(bmp->height-i-1), tmp, w);
 	}
 	free(tmp);
+
+	return 0;
+}
+
+int calc_rotation(DetectionDiff* diff, float *angles)
+{
+	if (diff->min_x > diff->max_x || diff->min_y > diff->max_y){
+		// no diff
+		return 1;
+	}
+	int max_x = diff->first_pass->width;
+	int max_y = diff->first_pass->height;
+
+	int center_x, center_y;
+
+	center_x = (diff->min_x + diff->max_x) / 2;
+	center_y = (diff->min_y + diff->max_y) / 2;
+
+	int orig_x = max_x / 2;
+	int orig_y = max_y / 2;
+
+	float relative_angle_hor, relative_angle_ver;
+
+	if (center_x == orig_x && center_y == orig_y){
+		// diff center is in origo
+		return 1;
+	}
+
+	if (center_x != orig_x){
+		float relative_angle_hor = center_x / (float) orig_x - 1.0f;
+	}
+	else{
+		relative_angle_hor = .0f;
+	}
+
+	if (center_y != orig_y){
+		float relative_angle_ver = center_y / (float) orig_y - 1.0f;
+	}
+	else{
+		relative_angle_ver = .0f;
+	}
+
+	angles[0] = (CAMERA_FOV_HORIZONTAL / 2.0f) * relative_angle_hor;
+	angles[1] = (CAMERA_FOV_VERTICAL / 2.0f) * relative_angle_ver;
 
 	return 0;
 }
--- src/camera.h
+++ src/camera.h
@@ -2,6 +2,7 @@
 #define CAMERA_H_
 
 #include "bmp.h"
+#include "motion_detect.h"
 
 /**
  * Takes a picture using raspistill.
@@ -9,5 +10,14 @@
  * @param bmp image where to save the taken image.
  */
 int take_pic(BMP *bmp);
+
+/**
+ * Calculates rotation angles from the center of motion difference.
+ *
+ * @param diff DetectionDiff to be used in calculation.
+ * @param angles two dimension float array that is filled with the results.
+ * Index 0 will contain horizontal angle and index 1 will contain vertical angle.
+ */
+int calc_rotation(DetectionDiff *diff, float *angles);
 
 #endif // CAMERA_H_
